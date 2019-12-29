1d8cd6afee98335c809280f15bd77006d0aab9aa
Douglas Black <dougblackjr@gmail.com>
2019-12-29 14:18:04 -0500

    Adding nav toggle

--- resources/views/partials/pre-nav.blade.php
+++ resources/views/partials/pre-nav.blade.php
@@ -2,7 +2,12 @@
     <div class="flex flex-shrink-0">
         <a class="text-xl text-indigo-500 font-semibold" href="/">Formman</a>
     </div>
-    <div class="flex flex-grow items-center w-full">
+    <div class="block lg:hidden">
+    	<button id="nav-button" class="flex items-center py-2 px-3 text-indigo-500 rounded border border-indigo-500">
+    		<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    	</button>
+    </div>
+    <div id="nav-dropdown" class="flex flex-grow items-center w-full">
         <div class="ml-auto">
             <a class="block lg:inline-block mt-4 lg:mt-0 lg:mr-8 text-blue-900 hover:text-blue-700" href="/login">Sign in</a>
             <a class="inline-block py-3 px-6 mt-4 lg:mt-0 leading-none text-white bg-indigo-500 hover:bg-indigo-600 rounded shadow" href="/register">Sign up</a>
